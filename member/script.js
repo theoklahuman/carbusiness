34b4428fd2a7e90debfce09a5bd1ce7faa532dc5
theoklahuman <theoklahuman@gmail.com>
2022-06-20 03:11:03 -0500

    Add JS to member page and make minor changes

---
+++ member/script.js
@@ -0,0 +1,5 @@
+const forgotPassword = document.querySelector(".forgot-password");
+
+forgotPassword.addEventListener("click", function(event) {
+    prompt("please enter your username/email address");
+})
