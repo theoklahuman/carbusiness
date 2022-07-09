90318e28dddb744f0de1c5a16b2a2eb85c81db68
theoklahuman <theoklahuman@gmail.com>
2022-07-09 11:21:53 -0500

    Make all pages responsive

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
-const button = document.querySelectorAll("button");
+const button = document.querySelector(".hamburger-button");
+const mainNavClass = document.querySelector(".main-nav");
+const closeHamburger = document.querySelector(".close-hamburger");
 
-console.log(button);
+button.addEventListener("click", function(event) {
+    if (mainNavClass.style.display = "none") {
+    mainNavClass.style.display = "flex";
+    button.style.display = "none";
+    closeHamburger.style.display = "block";
+    }
+})
+
+closeHamburger.addEventListener("click", function(event) {
+    if (mainNavClass.style.display = "flex") {
+        mainNavClass.style.display = "none";
+        button.style.display = "block";
+        closeHamburger.style.display = "none";
+        }
+})
+
+
+
+
